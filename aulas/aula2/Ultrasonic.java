4751e51c12284d3ef7b3cd7342dbd525f322d455
paulaksm <paulaksm@ime.usp.br>
2018-03-14 17:35:42 -0300

    Change to Ultrasonic

--- aulas/aula2/Ultrasonic.java
+++ aulas/aula2/Ultrasonic.java
@@ -1,3 +1,4 @@
+import lejos.robotics.navigation.DifferentialPilot;
 import lejos.nxt.UltrasonicSensor;
 import lejos.nxt.TouchSensor;
 import lejos.nxt.SensorPort;
@@ -8,13 +9,12 @@ public class Ultrasonic {
   public static void main(String[] args) {
     UltrasonicSensor sonic = new UltrasonicSensor(SensorPort.S4);
     TouchSensor touch = new TouchSensor(SensorPort.S1);
+    DifferentialPilot pilot = new DifferentialPilot(5.6f, 11.2f, Motor.B, Motor.C, false); //in cm
     while (!touch.isPressed()) {
       if (sonic.getDistance() > 30) {
-        Motor.B.forward();
-        Motor.C.forward();
+        pilot.forward();
       } else {
-        Motor.B.stop();
-        Motor.C.stop();
+        pilot.stop();
       }
     }
   }
